89c54a0335381fe2135df10cb358570f119c8796
sachsgit <25045377+sachsgit@users.noreply.github.com>
2021-12-18 22:05:13 -0500

    Trying landscaping in OODoc

--- src/main/perl/dummy.pl
+++ src/main/perl/dummy.pl
@@ -13,25 +13,30 @@ my $docFile = "../resources/landscape.odt";
 
 my $doc = odfDocument( file   => $docFile,
                        create => 'text' );
-my $pageLayout = $doc->updatePageLayout(
-                        "LandscapeStyle",
-                        properties => {
-                                        'fo:margin-bottom'          => '0.7874in',
-                                        'fo:page-width'             => '11in',
-                                        'style:footnote-max-height' => '0in',
-                                        'style:shadow'              => 'none',
-                                        'fo:margin-left'            => '0.7874in',
-                                        'fo:margin-right'           => '0.7874in',
-                                        'fo:page-height'            => '8.5in',
-                                        'style:num-format'          => '1',
-                                        'style:print-orientation'   => 'landscape',
-                                        'style:writing-mode'        => 'lr-tb',
-                                        'fo:margin-top'             => '0.7874in'
-                        }
+my $pageLayout = $doc->createPageLayout(
+                                         "LandscapeStyle",
+                                         properties => {
+                                                         'fo:margin-bottom'          => '0.7874in',
+                                                         'fo:page-width'             => '11.69"',
+                                                         'style:footnote-max-height' => '0in',
+                                                         'style:shadow'              => 'none',
+                                                         'fo:margin-left'            => '0.7874in',
+                                                         'fo:margin-right'           => '0.7874in',
+                                                         'fo:page-height'            => '8.27"',
+                                                         'style:num-format'          => '1',
+                                                         'style:print-orientation'   => 'landscape',
+                                                         'style:writing-mode'        => 'lr-tb',
+                                                         'fo:margin-top'             => '0.7874in'
+                                         }
 );
-$doc->switchPageOrientation($pageLayout);
+my $page = $doc->createMasterPage(
+                                   "LandscapeStyle",
+                                   layout => 'landscape',
+                                   next   => 'Standard'
+);
+$doc->switchPageOrientation($page);
 $doc->appendParagraph( text  => "Testing",
-                       style => $pageLayout );
+                       style => "LandscapeStyle" );
 
 $doc->save;
 print "\"$docFile\" is saved.\n";
